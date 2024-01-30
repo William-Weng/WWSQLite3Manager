b6d88d06705a322cd11c3bc8be46fb16e0e254db
William.Weng <William.Weng@iit.com.hk>
2024-01-30 16:16:59 +0800

    - SQL的常用函數的別名功能

--- Sources/WWSQLite3Manager/Function/SQLite3Method.swift
+++ Sources/WWSQLite3Manager/Function/SQLite3Method.swift
@@ -16,14 +16,14 @@ public extension SQLite3Method {
     // MARK: - SQL的常用函數 - Select
     enum SelectFunction {
         
-        case `default`(_ key: String, _ type: SQLite3Condition.DataType)            // 原始的一般欄位
-        case count(_ key: String? = nil, _ type: SQLite3Condition.DataType)         // 總數量
-        case distinct(_ key: String, _ type: SQLite3Condition.DataType)             // 未重複值
-        case distinctCount(_ key: String, _ type: SQLite3Condition.DataType)        // 未重複值的總數量
-        case min(_ key: String, _ type: SQLite3Condition.DataType)                  // 最小值
-        case max(_ key: String, _ type: SQLite3Condition.DataType)                  // 最大值
-        case avg(_ key: String, _ type: SQLite3Condition.DataType)                  // 平均值
-        case sum(_ key: String, _ type: SQLite3Condition.DataType)                  // 全部總和
+        case `default`(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)      // 原始的一般欄位
+        case count(_ key: String? = nil, _ type: SQLite3Condition.DataType, aliasName: String? = nil)   // 總數量
+        case distinct(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)       // 未重複值
+        case distinctCount(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)  // 未重複值的總數量
+        case min(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)            // 最小值
+        case max(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)            // 最大值
+        case avg(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)            // 平均值
+        case sum(_ key: String, _ type: SQLite3Condition.DataType, aliasName: String? = nil)            // 全部總和
         
         /// [產生SQL語句](https://ithelp.ithome.com.tw/articles/10208205)
         /// - Returns: [String](https://ithelp.ithome.com.tw/articles/10259378)
@@ -32,14 +32,14 @@ public extension SQLite3Method {
             var sql: String
             
             switch self {
-            case .default(let key, _): sql = key
-            case .count(let key, _): sql = "COUNT(*)"; if let key = key { sql = "COUNT(\(key))" }
-            case .distinct(let key, _): sql = "DISTINCT(\(key))"
-            case .distinctCount(let key, _): sql = "COUNT(DISTINCT(\(key)))"
-            case .min(let key, _): sql = "MIN(\(key))"
-            case .max(let key, _): sql = "MAX(\(key))"
-            case .avg(let key, _): sql = "AVG(\(key))"
-            case .sum(let key, _): sql = "SUM(\(key))"
+            case .default(let key, _, _): sql = key
+            case .count(let key, _, _): sql = "COUNT(*)"; if let key = key { sql = "COUNT(\(key))" }
+            case .distinct(let key, _, _): sql = "DISTINCT(\(key))"
+            case .distinctCount(let key, _, _): sql = "COUNT(DISTINCT(\(key)))"
+            case .min(let key, _, _): sql = "MIN(\(key))"
+            case .max(let key, _, _): sql = "MAX(\(key))"
+            case .avg(let key, _, _): sql = "AVG(\(key))"
+            case .sum(let key, _, _): sql = "SUM(\(key))"
             }
             
             sql += " as \(aliasName())"
@@ -54,14 +54,14 @@ public extension SQLite3Method {
             var aliasName: String
             
             switch self {
-            case .default(let key, _): aliasName = key
-            case .count(let key, _): aliasName = "Count"; if let key = key { aliasName = "\(key)Count" }
-            case .distinct(let key, _): aliasName = key
-            case .distinctCount(let key, _): aliasName = "\(key)DistinctCount"
-            case .min(let key, _): aliasName = "\(key)Min"
-            case .max(let key, _): aliasName = "\(key)Max"
-            case .avg(let key, _): aliasName = "\(key)Avg"
-            case .sum(let key, _): aliasName = "\(key)Sum"
+            case .default(let key, _, let _aliasName): aliasName = _aliasName ?? key
+            case .count(let key, _, let _aliasName): aliasName = "Count"; if let key = key { aliasName = _aliasName ?? "\(key)Count" }
+            case .distinct(let key, _, let _aliasName): aliasName = _aliasName ?? key
+            case .distinctCount(let key, _, let _aliasName): aliasName = _aliasName ?? "\(key)DistinctCount"
+            case .min(let key, _, let _aliasName): aliasName = _aliasName ?? "\(key)Min"
+            case .max(let key, _, let _aliasName): aliasName = _aliasName ?? "\(key)Max"
+            case .avg(let key, _, let _aliasName): aliasName = _aliasName ?? "\(key)Avg"
+            case .sum(let key, _, let _aliasName): aliasName = _aliasName ?? "\(key)Sum"
             }
             
             return aliasName
@@ -74,14 +74,14 @@ public extension SQLite3Method {
             var dataType: SQLite3Condition.DataType
             
             switch self {
-            case .default(_, let type): dataType = type
-            case .count(_, let type): dataType = type
-            case .distinct(_, let type): dataType = type
-            case .distinctCount(_, let type): dataType = type
-            case .min(_, let type): dataType = type
-            case .max(_, let type): dataType = type
-            case .avg(_, let type): dataType = type
-            case .sum(_, let type): dataType = type
+            case .default(_, let type, _): dataType = type
+            case .count(_, let type, _): dataType = type
+            case .distinct(_, let type, _): dataType = type
+            case .distinctCount(_, let type, _): dataType = type
+            case .min(_, let type, _): dataType = type
+            case .max(_, let type, _): dataType = type
+            case .avg(_, let type, _): dataType = type
+            case .sum(_, let type, _): dataType = type
             }
             
             return dataType
